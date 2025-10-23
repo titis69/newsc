1343eb4afe1458fdb6f45233d42a64c1236e06f2
titis69 <117496052+titis69@users.noreply.github.com>
2025-10-23 13:09:09 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -37,8 +37,8 @@ fun_bar() {
 }
 res1() {
     wget https://raw.githubusercontent.com/denisa32/sc/main/Cdy/menu.zip
-    wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/denisa32/sc/main/Enc/encrypt" ; chmod +x /usr/bin/enc
-    7z x -p@denisa32 menu.zip
+    wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/titis69/newsc/main/Enc/encrypt" ; chmod +x /usr/bin/enc
+    7z x -p@berlian69 menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu
